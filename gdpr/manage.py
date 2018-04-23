97907ec3cbb74732aca97d92553b2c29074bdede
Clement Denoix <clement.denoix@algolia.com>
2018-04-23 10:36:38 +0200

    Add path to manage.py

--- gdpr/manage.py
+++ gdpr/manage.py
@@ -3,6 +3,8 @@ import os
 import sys
 
 if __name__ == "__main__":
+    path = os.path.dirname(os.path.abspath(__file__))
+    sys.path.append(path)
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gdpr.settings")
     try:
         from django.core.management import execute_from_command_line
