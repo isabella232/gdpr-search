ca3d278c5b44ede3a7d54f1d1c44bbbce2d998f6
Clement Denoix <clement.denoix@algolia.com>
2018-04-26 15:22:47 +0200

    feat(locale) Redirect based on the users language

--- gdpr/settings/base.py
+++ gdpr/settings/base.py
@@ -58,8 +58,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.cache.FetchFromCacheMiddleware'
+    'django.middleware.locale.LocaleMiddleware',
 ]
 
 ROOT_URLCONF = 'gdpr.urls'
